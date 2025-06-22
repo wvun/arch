783e2f9a51941ee035948bbc441d39fcbdda815e
wvun <mdlschifino@gmail.com>
2025-06-22 19:59:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simula loading e mostra a tela principal
+setTimeout(() => {
+  document.getElementById('loader').classList.add('hidden');
+  document.getElementById('mainContent').classList.remove('hidden');
+}, 2000);
